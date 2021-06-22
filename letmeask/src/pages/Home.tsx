d699c562f6f01342759d157f00e99fc9aba7ab9a
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-21 23:31:39 -0300

    criando hook de autenticaçao

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,18 +1,20 @@
 import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { AuthContext } from '../App';
+import { AuthContext } from '../contexts/AuthContext';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import '../styles/auth.scss';
 import Button from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
 
 export default function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
-import '../styles/auth.scss';
 import Button from '../components/Button';
-import { AuthContext } from '../App';
+// import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
 
 export default function NewRoom() {
-  const { user } = useContext(AuthContext);
+  // const { user } = useAuth;
 
   return (
     <div id="page-auth">
@@ -24,7 +24,6 @@ export default function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <h1>{user?.name}</h1>
           <h2>Crie uma nova sala</h2>
           <form>
             <input type="text" placeholder="Nome da sala" />
